681fd5469955ea89a27915a8935bc3a27f6d07f3
KanikeAshritha <ashritha.kanike@minfytech.com>
2025-05-14 09:58:17 +0530

    initial commit

---
+++ functions/functions_solutions.py
@@ -0,0 +1,93 @@
+#easy1
+def calculate_average(nums):
+    avg=0
+    if len(nums)==0:
+        return 0
+    else:
+        avg=sum(nums)/len(nums)
+        return avg
+print("easy1")
+print(calculate_average([5, 10, 15, 20]))  
+print(calculate_average([]))
+
+
+
+
+#easy2
+def greet_user(name,*args):
+    if(args):
+        for a in args:
+            return f"{a}, {name}!"
+    else:
+        return f"Hello, {name}!"
+print("easy2")
+print(greet_user("Alice")) 
+print(greet_user("Bob", "Hi"))  
+
+
+
+
+#medium1
+def calculate_total(*args,discount=0):
+    total_cost=0
+    for a in args:
+        total_cost += a
+    if(discount!=0):
+        total_cost -= (total_cost*discount)/100
+    return int(total_cost)
+
+print("medium1")
+print(calculate_total(10, 20, 30)) 
+print(calculate_total(10, 20, 30, discount=10)) 
+
+
+
+
+#medium2
+def create_multiplier(n):
+    def func(x):
+        return x*n
+    return func
+double = create_multiplier(2)
+triple = create_multiplier(3)
+print("medium2")
+print(double(5))  
+print(triple(5))
+
+
+
+#hard1
+def power(x,n):
+    if n == 0:
+        return 1
+    if n < 0 :
+        x = 1 / x
+        n = -n
+    if n%2 == 0:
+        pow1 = power(x , n // 2)
+        return pow1*pow1
+    else:
+        return x* x**(n-1)
+print("hard1")
+print(power(2, 10)) 
+print(power(3, 4))
+
+
+
+
+
+#hard2
+def compose(*args):
+    def composed(x):
+        for func in reversed(args):
+            x= func(x)
+        return x
+    return composed
+
+def add_one(x): return x + 1
+def double(x): return x * 2
+def square(x): return x ** 2
+
+f = compose(square, double, add_one)
+print("hard2")
+print(f(3)) 
